bbbaf6a97566805469c3470bcaf31f6143ec661f
Omkar5053 <omkar62006@gmail.com>
2023-05-25 14:10:28 +0530

    finalized the things

--- strikingdash-html/src/scripts/hostel.js
+++ strikingdash-html/src/scripts/hostel.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  
+  $("#popup-message").hide();
   let userId = localStorage.getItem("userId");
   let roleType = localStorage.getItem("roleType");
   let username = localStorage.getItem("username");
@@ -9,46 +11,53 @@ $(document).ready(function () {
   getHostels();
   function getHostels() {
     $.ajax({
-      url: `http://localhost:8080/hostel/getAllHostels`,
+      url: `http://localhost:8080/hostel/getAllHostels?userId=${userId}&roleType=${roleType}`,
       type: "POST",
       beforeSend: function () {},
-      success: function (data, status) {
-        $("#hostelTableDataBody").empty();
-        for (i = 0; i < data.length; i++) {
-          $("#hostelTableDataBody").append(
-            $("<tr>")
-              .append(
-                $("<td>").append(
-                  $("<div>")
-                    .addClass("userDatatable-content")
-                    .append(i + 1)
+      success: function (data) {
+        if(data.status == false)
+        {
+          $(location).attr("href", "login.html");
+        } 
+        else{
+          $("#hostelTableDataBody").empty();
+          for (i = 0; i < data.listOfData.length; i++) {
+            $("#hostelTableDataBody").append(
+              $("<tr>")
+                .append(
+                  $("<td>").append(
+                    $("<div>")
+                      .addClass("userDatatable-content")
+                      .append(i + 1)
+                  )
                 )
-              )
-              .append(
-                $("<td>").append(
-                  $("<div>")
-                    .addClass("userDatatable-content")
-                    .append(data[i].hostelName)
+                .append(
+                  $("<td>").append(
+                    $("<div>")
+                      .addClass("userDatatable-content")
+                      .append(data.listOfData[i].hostelName)
+                  )
                 )
-              )
-              .append(
-                $("<td>").append(
-                  `<i  style="font-size:20px; cursor:pointer; margin:3px;" class="far fa-edit editHostel" id=""  data-name="` +
-                    data[i].hostelName +
-                    `" data-aid="` +
-                    data[i].hostel_id +
-                    `"></i>
-                       <i class="fas fa-trash deleteHostel" style="font-size:20px; cursor:pointer; margin:3px;" data-aid="` +
-                    data[i].hostel_id +
-                    `"></i>
-                 `
+                .append(
+                  $("<td>").append(
+                    `<i  style="font-size:20px; cursor:pointer; margin:3px;" class="far fa-edit editHostel" id=""  data-name="` +
+                      data.listOfData[i].hostelName +
+                      `" data-aid="` +
+                      data.listOfData[i].hostel_id +
+                      `"></i>
+                         <i class="fas fa-trash deleteHostel" style="font-size:20px; cursor:pointer; margin:3px;" data-aid="` +
+                      data.listOfData[i].hostel_id +
+                      `"></i>
+                   `
+                  )
                 )
-              )
-          );
+            );
+          }
         }
+        
       },
       error: function () {
-        console.log("Eror");
+        $(location).attr("href", "login.html");
       },
     });
 
@@ -101,51 +110,35 @@ $(document).ready(function () {
   });
 
   $("#addHostel").click(function () {
-    let data = {
-      hostelName: $("#addHostelName").val(),
-    };
-    if (data.hostelName == "") {
-      // <button class="btn btn-outline-lighten fs-14 fw-500 btn-lg btn-message" data-messageType="warning" data-messageIcon="exclamation-circle">Warning</button>
+    if ($("#addHostelName").val() == "") {
+       alert("Please enter the hostel name")
     } else {
       let userId = localStorage.getItem("userId");
       let roleType = localStorage.getItem("roleType");
+      let hostelName = $("#addHostelName").val();
+      let hostelId = 0
       $.ajax({
-        url: `http://localhost:8080/hostel/addHostel?userId=${userId}&roleType=${roleType}`,
+        url: `http://localhost:8080/hostel/addHostel?userId=${userId}&roleType=${roleType}&hostelName=${hostelName}&hostelId=${hostelId}`,
         headers: {
           "Content-Type": "application/json",
         },
         type: "POST",
         dataType: "json",
-        data: JSON.stringify(data),
         success: function (data) {
+          $("#popup-message").hide();
+          console.log(data);
           $("#addHostelName").val("");
-          //console.log(data);
           getHostels();
           window.location.reload();
         },
-        error: function () {
-          console.log("Error");
+        error: function (error) {
+          $("#popup-message").show();
         },
       });
     }
   });
 
-  // function deleteConfirm(cId) {
-  //   swal({
-  //     title: "Are you sure?",
-  //     text: "you want to delete this contact",
-  //     icon: "warning",
-  //     buttons: true,
-  //     dangerMode: true,
-  //   }).then((willDelete) => {
-  //     if (willDelete) {
-  //       window.location = "/user/delete/" + cId;
-  //     } else {
-  //       swal("Your contact is safe");
-  //     }
-  //   });
-  // }
-
+  
   function deleteHostel(hostelId) {
     swal({
       title: "Are you sure?",
@@ -180,5 +173,66 @@ $(document).ready(function () {
       //   swal("Your Hostel is safe");
       // }
     });
+  }
+
+
+
+
+
+
+
+
+
+
+  const msgButtons = document.querySelectorAll('.btn-message');
+  let messageCount = 0;
+  function createMessage(type,icon,loading){
+    let message = ``;
+    const messageShowcase = $('.message-wrapper');
+    if(!loading){
+      message = `
+        <div class="atbd-pop-message message-${type} message-${messageCount}">
+            <span class="atbd-pop-message__icon">
+                <i class="la la-${icon}"></i>
+            </span>
+            <span class="atbd-pop-message__text"><p>Hostel has already registered</p></span>
+        </div>
+      `;
+    }else{
+      message = `
+        <div class="atbd-pop-message message-${type} message-${messageCount}">
+            <span class="atbd-pop-message__icon">
+                <i class="la la-loader"></i>
+            </span>
+            <span class="atbd-pop-message__text"><p>Hostel has already registered</p></span>
+        </div>
+    `;
+    }
+    
+    messageShowcase.append(message);
+    messageCount++;
+  }
+  function showMessage(e){
+    e.preventDefault();
+    let duration = (optionValue, defaultValue) =>
+      typeof optionValue === "undefined" ? defaultValue : optionValue;
+
+
+    
+    let messageType = this.dataset.messagetype;
+    let messageIcon = this.dataset.messageicon;
+    let messageLoading = this.dataset.messageLoading;
+    createMessage(messageType,messageIcon,messageLoading);
+    feather.replace();
+    let thismessage = messageCount - 1;
+
+    setTimeout(function(){
+      $(document).find(".message-"+thismessage).remove();
+    },duration(this.dataset.duration,3000));
+
+  }
+
+  if(msgButtons){
+    msgButtons.forEach(messageButton => messageButton.addEventListener('click',showMessage));
   }
 });
